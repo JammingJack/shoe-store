373db44c58d0e73603e47873e28bd9962aba40d5
JammingJack <amineseth1@gmail.com>
2021-10-17 20:46:50 +0100

    Added 404 page for invalid url product id

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import useFetch from "./services/useFetch";
 import { useParams } from "react-router-dom";
 import Spinner from "./Spinner";
+import PageNotFound from "./PageNotFound";
 
 export default function Detail() {
   const { id } = useParams();
@@ -10,6 +11,7 @@ export default function Detail() {
   //TODO Display Product Detail
   if (error) throw error;
   if (loading) return <Spinner />;
+  if (!product) return <PageNotFound />;
   return (
     <div>
       <h1>{product.name}</h1>
