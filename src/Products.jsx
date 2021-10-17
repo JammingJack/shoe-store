48cce6ea99b2a0085eea30edd76b524b72469277
JammingJack <amineseth1@gmail.com>
2021-10-17 15:39:00 +0100

    Refactoring: moved products displaying code from App.jsx to separate component Products.jsx

---
+++ src/Products.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import Spinner from "./Spinner";
+import useFetch from "./services/useFetch";
+
+export default function App() {
+  const [size, setSize] = useState("");
+  const {
+    data: products,
+    error,
+    loading,
+  } = useFetch("products?category=shoes");
+
+  const filteredProducts = size
+    ? products.filter((product) =>
+        product.skus.includes((item) => item.size === parseInt(size))
+      )
+    : products;
+
+  function renderProduct(p) {
+    return (
+      <div key={p.id} className="product">
+        <a href="/">
+          <img src={`/images/${p.image}`} alt={p.name} />
+          <h3>{p.name}</h3>
+          <p>${p.price}</p>
+        </a>
+      </div>
+    );
+  }
+
+  if (error) throw error;
+  if (loading) return <Spinner />; //show spinner if is loading
+  return (
+    <>
+      <section id="filters">
+        <label htmlFor="size">Filter by Size:</label>{" "}
+        <select
+          id="size"
+          value={size}
+          onChange={(ev) => {
+            setSize(ev.target.value);
+          }}
+        >
+          <option value="">All sizes</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+        </select>
+        {size && <h2>Found {filteredProducts.length} items.</h2>}
+      </section>
+      <section id="products">{filteredProducts.map(renderProduct)}</section>
+    </>
+  );
+}
