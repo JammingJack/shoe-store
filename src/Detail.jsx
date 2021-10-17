fddd1e69b12163d55e01d56ea3e71b50896db511
JammingJack <amineseth1@gmail.com>
2021-10-17 20:55:29 +0100

    enabled redirecting to cart when clicking on AddToCart on Detail page

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import useFetch from "./services/useFetch";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import Spinner from "./Spinner";
 import PageNotFound from "./PageNotFound";
 
 export default function Detail() {
   const { id } = useParams();
   const { data: product, error, loading } = useFetch(`products/${id}`);
-
+  const Navigate = useNavigate();
   //TODO Display Product Detail
   if (error) throw error;
   if (loading) return <Spinner />;
@@ -17,6 +17,11 @@ export default function Detail() {
       <h1>{product.name}</h1>
       <p>{product.description}</p>
       <p id="price">${product.price}</p>
+      <p>
+        <button className="btn btn-primary" onClick={() => Navigate("/cart")}>
+          Add to cart
+        </button>
+      </p>
       <img src={`/images/${product.image}`} alt={product.category} />
     </div>
   );
