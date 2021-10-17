a739062405e6b689ddd452dbb507ea77b46422f7
JammingJack <amineseth1@gmail.com>
2021-10-17 18:39:36 +0100

    implemented client-side Nav and NavLink

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,27 @@
 import React from "react";
-
+import { Link, NavLink } from "react-router-dom";
+const activeStyle = {
+  color: "purple",
+};
 export default function Header() {
   return (
     <header>
       <nav>
         <ul>
           <li>
-            <img alt="Carved Rock Fitness" src="/images/logo.png" />
+            <Link to="/">
+              <img alt="Carved Rock Fitness" src="/images/logo.png" />
+            </Link>
+          </li>
+          <li>
+            <NavLink to="/shoes" activeClassName={activeStyle}>
+              Shoes
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/cart" activeClassName={activeStyle}>
+              Cart
+            </NavLink>
           </li>
         </ul>
       </nav>
