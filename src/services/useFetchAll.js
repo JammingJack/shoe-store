9849f3a583f48b133f385a71a9b46eecaa24a259
JammingJack <amineseth1@gmail.com>
2022-02-09 17:15:47 +0100

    fixed bugs : same as previous bug, but in useFetchAll, other bug is infinite loop caused by urls dependancie in useEffect, fixed by comparing urls from urls of previous render

--- src/services/useFetchAll.js
+++ src/services/useFetchAll.js
@@ -1,11 +1,18 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export default function useFetchAll(urls) {
+  const previousUrls = useRef([]); //ref to previous render's urls
+  const isMounted = useRef(false);
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    isMounted.current = true;
+    if (areEqual(urls, previousUrls.current)) {
+      setLoading(false);
+      return;
+    }
     const promises = urls.map((url) =>
       fetch(process.env.REACT_APP_API_BASE_URL + url).then((response) => {
         if (response.ok) return response.json();
@@ -14,14 +21,27 @@ export default function useFetchAll(urls) {
     );
 
     Promise.all(promises)
-      .then((json) => setData(json))
+      .then((json) => {
+        if (isMounted.current) setData(json);
+      })
       .catch((e) => {
         console.error(e);
-        setError(e);
+        if (isMounted.current) setError(e);
       })
-      .finally(() => setLoading(false));
-    // eslint-disable-next-line
-  }, []);
+      .finally(() => {
+        if (isMounted.current) setLoading(false);
+      });
+    return () => {
+      isMounted.current = false;
+    };
+  }, [urls]); //added urls because re-render is dependant on the list of urls received //bug : infinite loop is list of urls not checked for change first
 
   return { data, loading, error };
+}
+
+function areEqual(arr1, arr2) {
+  return (
+    arr1.length === arr2.length &&
+    arr1.every((value, index) => value === arr2[index])
+  );
 }
